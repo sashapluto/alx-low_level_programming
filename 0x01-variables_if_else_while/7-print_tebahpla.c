7a359ac4549706b32bb8b3973b00f9aaea840c50
sashapluto <mbanilasasha@gmail.com>
2022-12-12 10:39:31 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
+
