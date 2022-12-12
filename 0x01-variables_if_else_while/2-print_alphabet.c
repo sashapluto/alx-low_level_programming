fb05a3e502e7ecac6f4cc83c4b35561b39221e26
sashapluto <mbanilasasha@gmail.com>
2022-12-12 08:49:37 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
